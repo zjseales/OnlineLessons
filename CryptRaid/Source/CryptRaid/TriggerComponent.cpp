7de395a7f8d98b3c6df9219d3eae02bcaaa28d0f
DESKTOP-DAST5UK\Zac <zac_seales@live.com>
2023-02-28 16:09:04 +1300

    First Skeleton puzzle triggers an unlock method when both keys are placed.

--- CryptRaid/Source/CryptRaid/TriggerComponent.cpp
+++ CryptRaid/Source/CryptRaid/TriggerComponent.cpp
@@ -1,5 +1,6 @@
 
 #include "TriggerComponent.h"
+#include "GameFramework/Actor.h"
 
 UTriggerComponent::UTriggerComponent()
 {
@@ -20,13 +21,55 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+    bool KeysComplete = CheckKeys();
+
+    if (KeysComplete)
+    {
+        UnlockDoor();
+    }
+
+}
+
+// returns true if both keys have been placed.
+bool UTriggerComponent::CheckKeys()
+{
     TArray<AActor*> Actors;
     GetOverlappingActors(Actors);
 
-    if(Actors.Num() > 0)
-    {
-        FString OverlappingActor = Actors[0]->GetActorNameOrLabel();
-        UE_LOG(LogTemp, Display, TEXT("Actor overlapping: %s"), *OverlappingActor);
-    }
+    int32 keys = 0;
 
+    // iterate all Actors in the trigger box bounds.
+    for(AActor* Actor: Actors)
+    {
+        //should put this in a reusable method
+        if(Actor->ActorHasTag("Arm1"))
+        {
+            FVector ArmLocation = FVector(-1739.252079, 2400.368324, 74.149317);
+            FRotator ArmRotation = FRotator(31.910073, 6.303607, 101.803848);
+            UPrimitiveComponent* ArmPrimitive = Cast<UPrimitiveComponent>(Actor->GetRootComponent());
+            ArmPrimitive->SetSimulatePhysics(false);
+            Actor->SetActorLocationAndRotation(ArmLocation, ArmRotation);
+            keys++;
+        }
+        if(Actor->ActorHasTag("Leg1"))
+        {
+            FVector LegLocation = FVector(-1670.876809, 2407.398451, 44.243646);
+            FRotator LegRotation = FRotator(68.010444, -13.864153, -84.340322);
+            UPrimitiveComponent* LegPrimitive = Cast<UPrimitiveComponent>(Actor->GetRootComponent());
+            LegPrimitive->SetSimulatePhysics(false);
+            Actor->SetActorLocationAndRotation(LegLocation, LegRotation);
+            keys++;
+        }
+    }
+    if(keys == 2)
+    {
+        return true;
+    }
+    return false;
+}
+
+// unlocks the secret door
+void UTriggerComponent::UnlockDoor()
+{
+    UE_LOG(LogTemp, Display, TEXT("Both Keys Placed, Unlocking..."));
 }
--- CryptRaid/Source/CryptRaid/TriggerComponent.h
+++ CryptRaid/Source/CryptRaid/TriggerComponent.h
@@ -25,5 +25,9 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	bool CheckKeys();
+	void UnlockDoor();
 	
 };
