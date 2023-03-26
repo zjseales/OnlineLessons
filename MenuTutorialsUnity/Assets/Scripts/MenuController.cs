5a5a3122f108fda97c12d05acde147948e80b5d3
DESKTOP-DAST5UK\Zac <zac_seales@live.com>
2023-03-26 19:06:04 +1300

    Resolution options now change with button press. Built a test project and found some alignment issues.

--- MenuTutorialsUnity/Assets/Scripts/MenuController.cs
+++ MenuTutorialsUnity/Assets/Scripts/MenuController.cs
@@ -35,18 +35,35 @@ public class MenuController : MonoBehaviour
         buttonPanel.SetActive(true);
     }
 
-    public void setQuality()
-    {
-        changeQuality();
-    }
-
-    // changes video quality dependent on the quality button clicked
-    void changeQuality()
+    // change screen resolution dependent on name of button that was pressed
+    public void setResolution()
     {
         //retrieve name of clicked item
         string level = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
 
-        switch (level) 
+        switch (level)
+        {
+            case "LowRes":
+                Screen.SetResolution(1152, 648, true);
+                break;
+            case "MidRes":
+                Screen.SetResolution(1200, 720, true);
+                break;
+            case "HighRes":
+                Screen.SetResolution(1360, 764, true);
+                break;
+            case "EpicRes":
+                Screen.SetResolution(1920, 1080, true);
+                break;
+        }
+    }
+
+    public void setQuality()
+    {
+        //retrieve name of clicked item
+        string level = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        switch (level)
         {
             case "Low":
                 QualitySettings.SetQualityLevel(0);
@@ -61,7 +78,7 @@ public class MenuController : MonoBehaviour
                 QualitySettings.SetQualityLevel(3);
                 break;
             case "Shadows":
-                if(QualitySettings.shadows == ShadowQuality.All)
+                if (QualitySettings.shadows == ShadowQuality.All)
                 {
                     QualitySettings.shadows = ShadowQuality.Disable;
                 }
