d4c7fe4311eebbaa5d901cba852856eefa0167aa
DESKTOP-DAST5UK\Zac <zac_seales@live.com>
2023-06-04 16:55:16 +1200

    Constructing a Base Pawn class.

--- ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/BasePawn.h
@@ -17,6 +17,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp; 
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
