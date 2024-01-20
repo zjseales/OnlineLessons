a87bba7b145809a530ac7139c93143757991e823
zjseales <saczeales@gmail.com>
2024-01-20 20:03:15 +1300

    Forward Declared UCapsuleComponent.

--- ToonTanks/Source/ToonTanks/BasePawn.cpp
+++ ToonTanks/Source/ToonTanks/BasePawn.cpp
@@ -1,10 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "BasePawn.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true; 
+	PrimaryActorTick.bCanEverTick = true;
 
 }
 
--- ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanks/Source/ToonTanks/BasePawn.h
@@ -1,3 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -19,9 +21,9 @@ protected:
 
 private:
 	UPROPERTY()
-	class UCapsuleComponent* CapsuleComp; 
+	class UCapsuleComponent* CapsuleComp;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
