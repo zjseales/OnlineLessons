567c7e2e69f56a89f6276bff936f88ff528950fa
zjseales <saczeales@gmail.com>
2024-01-29 15:43:45 +1300

    Bind methods to axis mappings for player input.

--- ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanks/Source/ToonTanks/BasePawn.h
@@ -33,7 +33,5 @@ private:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
