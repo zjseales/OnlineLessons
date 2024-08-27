dcf96c3c63b164230ba2d80bbd9dc4c693d083c5
DESKTOP-U2LPBFU\Zac <saczeales@gmail.com>
2024-08-27 16:58:05 +1200

    Rebuilt in latest engine. Created Enemy "Tower" class.

---
+++ ToonTanks/Source/ToonTanks/Tower.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/** Tower.h
+ * 	
+ * 	Enemy towers that rotate toward, and attack the player pawn.
+ *  @author Zac Seales (taken from gamedev.tv "ToonTanks" lessons)
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
