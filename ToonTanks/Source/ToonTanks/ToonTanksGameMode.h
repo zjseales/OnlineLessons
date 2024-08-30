8f8e1db5a8ed97b9f0542b6f9d34fa670ceb781f
DESKTOP-U2LPBFU\Zac <saczeales@gmail.com>
2024-08-30 16:26:27 +1200

    Created and applied custom game mode.

---
+++ ToonTanks/Source/ToonTanks/ToonTanksGameMode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToonTanksGameMode.generated.h"
+
+/** ToonTanksGameMode.h
+ * 	
+ * 	Defines rules of the ToonTanks game.
+ *  Adapted from gamedev.tv "ToonTanks" lessons. 
+ */
+
+UCLASS()
+class TOONTANKS_API AToonTanksGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
