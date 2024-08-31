55538478a4449be952af3cd67552fa7b80827fa8
DESKTOP-U2LPBFU\Zac <saczeales@gmail.com>
2024-08-31 14:56:17 +1200

    Set up custom player controller.

---
+++ ToonTanks/Source/ToonTanks/ToonTanksPlayerController.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ToonTanksPlayerController.generated.h"
+
+/** ToonTanksPlayerController.h
+ * 
+ *  Custom Player controller for ToonTanks project.
+ *  - derived from gamedev.tv lessons. 
+ */
+UCLASS()
+class TOONTANKS_API AToonTanksPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+ 
+	void SetPlayerEnabledState(bool bPlayerEnabled);
+	
+};
