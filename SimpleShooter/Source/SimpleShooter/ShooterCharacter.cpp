f15f3a95a967f908d62efc363c29b2e4694aa923
DESKTOP-U2LPBFU\Zac <saczeales@gmail.com>
2024-09-10 14:31:25 +1200

    Duplicated sandbox map for editing. Created game mode and main character objects.

---
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
@@ -0,0 +1,31 @@
+#include "ShooterCharacter.h"
+
+// Constructor for a shooter character.
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when this object is constructed.
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
